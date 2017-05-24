c00a2bc450d2f7ba05432362e206bf4c20abe0bf
Quinn <quinngunter.qg@gmail.com>
2017-05-24 11:30:28 -0600

    Updated UI

--- Project 5.1 View Animations/ViewController.swift
+++ Project 5.1 View Animations/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     //Blurr
     @IBOutlet weak var blurPhotoView: UIVisualEffectView!
     @IBOutlet weak var welcomText: UIVisualEffectView!
@@ -28,26 +28,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var emailFieldTrailing: NSLayoutConstraint!
     @IBOutlet weak var emailFieldLeading: NSLayoutConstraint!
     
-    override func viewWillAppear(_ animated: Bool) {
-        //Login
-        loginLabelTop.constant = -view.bounds.height
+    //LoginButton
+    @IBOutlet weak var setLoginButton: UIButton!
+    @IBAction func loginButton(_ sender: UIButton) {
+        //Login BUtton disapear
+        UIView.animate(withDuration: 0.5) {
+            self.setLoginButton.alpha = 0
+        }
         
-        //Name Field Off Screen
-        nameFieldLeading.constant = view.bounds.width
-        nameFieldTrailing.constant = -view.bounds.width
-        
-        //Email Field off screen
-        emailFieldLeading.constant = -view.bounds.width
-        emailFieldTrailing.constant = view.bounds.width
-        
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
         //Blur
         UIView.animate(withDuration: 2) {
             self.blurPhotoView.alpha = 1
@@ -76,6 +64,28 @@ class ViewController: UIViewController {
         }, completion: { _ in
             self.emailTextField.becomeFirstResponder()
         })
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        //Login
+        loginLabelTop.constant = -view.bounds.height
+        
+        //Name Field Off Screen
+        nameFieldLeading.constant = view.bounds.width
+        nameFieldTrailing.constant = -view.bounds.width
+        
+        //Email Field off screen
+        emailFieldLeading.constant = -view.bounds.width
+        emailFieldTrailing.constant = view.bounds.width
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setLoginButton.layer.cornerRadius = 10
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         
     }
     
