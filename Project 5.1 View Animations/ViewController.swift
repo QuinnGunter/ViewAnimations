4228a1de3c7271b6809af07a0e7a67fbcfbc7b74
Quinn <quinngunter.qg@gmail.com>
2017-05-24 11:17:24 -0600

    Basic View animations

--- Project 5.1 View Animations/ViewController.swift
+++ Project 5.1 View Animations/ViewController.swift
@@ -9,12 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Blurr
+    @IBOutlet weak var blurPhotoView: UIVisualEffectView!
+    @IBOutlet weak var welcomText: UIVisualEffectView!
+    
+    //Login Label
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var loginLabelTop: NSLayoutConstraint!
+    
+    //Name Text Field
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameFieldTrailing: NSLayoutConstraint!
+    @IBOutlet weak var nameFieldLeading: NSLayoutConstraint!
+    
+    //Email Text Field
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailFieldTrailing: NSLayoutConstraint!
+    @IBOutlet weak var emailFieldLeading: NSLayoutConstraint!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //Login
+        loginLabelTop.constant = -view.bounds.height
+        
+        //Name Field Off Screen
+        nameFieldLeading.constant = view.bounds.width
+        nameFieldTrailing.constant = -view.bounds.width
+        
+        //Email Field off screen
+        emailFieldLeading.constant = -view.bounds.width
+        emailFieldTrailing.constant = view.bounds.width
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //Blur
+        UIView.animate(withDuration: 2) {
+            self.blurPhotoView.alpha = 1
+        }
+        
+        //Login Spring top
+        UIView.animate(withDuration: 1, delay: 1, usingSpringWithDamping: 1, initialSpringVelocity: 15, options: [], animations: {
+            self.loginLabelTop.constant = 180
+            self.view.layoutIfNeeded()
+        })
+        
+        //Name Spring in
+        UIView.animate(withDuration: 1, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 15, options: [], animations: {
+            self.nameFieldLeading.constant  = 16
+            self.nameFieldTrailing.constant = 16
+            self.view.layoutIfNeeded()
+        }, completion: { _ in
+            self.nameTextField.becomeFirstResponder()
+        })
+        
+        //Email Spring in
+        UIView.animate(withDuration: 1, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 15, options: [], animations: {
+            self.emailFieldLeading.constant  = 16
+            self.emailFieldTrailing.constant = 16
+            self.view.layoutIfNeeded()
+        }, completion: { _ in
+            self.emailTextField.becomeFirstResponder()
+        })
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
